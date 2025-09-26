4044a68aae267535211a9fddfe5dbee4e9e7ebaf
ShijasMkt <shijumkt@gmail.com>
2025-09-26 15:04:29 +0530

    firebase messaging initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:developer';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -8,12 +10,19 @@ import 'package:spendly/core/constants/app_themes.dart';
 import 'package:spendly/featues/category/data/models/category_model.dart';
 import 'package:spendly/featues/expense/data/models/expense_model.dart';
 
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message)async{
+  await Firebase.initializeApp();
+  log("Handling background message: ${message.messageId}");
+}
+
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   //firebase initialization
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform
   );
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   //Hive initialization
   await Hive.initFlutter();
   //Hive adapter registeration
